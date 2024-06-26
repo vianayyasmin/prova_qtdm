9eeab1bd934fc9ee90537c41fd3b743549abe849
Yasmin Viana <127878560+vianayyasmin@users.noreply.github.com>
2024-06-26 11:21:29 -0300

    Create criarproduto.dart

---
+++ criarproduto.dart
@@ -0,0 +1,255 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'criar_produto_model.dart';
+export 'criar_produto_model.dart';
+
+class CriarProdutoWidget extends StatefulWidget {
+  const CriarProdutoWidget({super.key});
+
+  @override
+  State<CriarProdutoWidget> createState() => _CriarProdutoWidgetState();
+}
+
+class _CriarProdutoWidgetState extends State<CriarProdutoWidget> {
+  late CriarProdutoModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => CriarProdutoModel());
+
+    _model.textController ??= TextEditingController();
+    _model.textFieldFocusNode ??= FocusNode();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: AlignmentDirectional(0, 0),
+      child: Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 16),
+        child: Container(
+          constraints: BoxConstraints(
+            maxWidth: 570,
+            maxHeight: 260,
+          ),
+          decoration: BoxDecoration(
+            color: FlutterFlowTheme.of(context).secondaryBackground,
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(
+              color: Color(0xFFE0E3E7),
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 12, 0),
+                        child: Text(
+                          'Adicionar Produto',
+                          style: FlutterFlowTheme.of(context)
+                              .headlineMedium
+                              .override(
+                                fontFamily: 'Poppins',
+                                color: Color(0xFFFF8BBB),
+                                letterSpacing: 0,
+                              ),
+                        ),
+                      ),
+                    ),
+                    FlutterFlowIconButton(
+                      borderColor:
+                          FlutterFlowTheme.of(context).primaryBackground,
+                      borderRadius: 30,
+                      borderWidth: 2,
+                      buttonSize: 44,
+                      icon: Icon(
+                        Icons.close_rounded,
+                        color: FlutterFlowTheme.of(context).secondaryText,
+                        size: 24,
+                      ),
+                      onPressed: () async {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                ),
+                Divider(
+                  height: 24,
+                  thickness: 2,
+                  color: FlutterFlowTheme.of(context).primaryBackground,
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 12),
+                  child: TextFormField(
+                    controller: _model.textController,
+                    focusNode: _model.textFieldFocusNode,
+                    autofocus: true,
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      labelText: 'Nome do produto',
+                      hintText: 'Insira o nome do produto',
+                      hintStyle: FlutterFlowTheme.of(context)
+                          .bodySmall
+                          .override(
+                            fontFamily: 'Poppins',
+                            color: FlutterFlowTheme.of(context).secondaryText,
+                            fontSize: 15,
+                            letterSpacing: 0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: FlutterFlowTheme.of(context).accent3,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0x00000000),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0x00000000),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0x00000000),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      contentPadding:
+                          EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                    ),
+                    style: FlutterFlowTheme.of(context).bodyMedium.override(
+                          fontFamily: 'Poppins',
+                          color: FlutterFlowTheme.of(context).primaryText,
+                          letterSpacing: 0,
+                        ),
+                    validator:
+                        _model.textControllerValidator.asValidator(context),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FFButtonWidget(
+                        onPressed: () async {
+                          Navigator.pop(context);
+                        },
+                        text: 'Cancelar',
+                        options: FFButtonOptions(
+                          width: 120,
+                          height: 44,
+                          padding: EdgeInsets.all(0),
+                          iconPadding:
+                              EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                          color:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                          textStyle:
+                              FlutterFlowTheme.of(context).bodySmall.override(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 14,
+                                    letterSpacing: 0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                          elevation: 0,
+                          borderSide: BorderSide(
+                            color:
+                                FlutterFlowTheme.of(context).primaryBackground,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(100),
+                          hoverColor:
+                              FlutterFlowTheme.of(context).primaryBackground,
+                          hoverTextColor:
+                              FlutterFlowTheme.of(context).primaryText,
+                        ),
+                      ),
+                      FFButtonWidget(
+                        onPressed: () async {
+                          await ProdutosRecord.collection
+                              .doc()
+                              .set(createProdutosRecordData(
+                                produto: 'Nome do Produto',
+                                comprado: false,
+                              ));
+                        },
+                        text: 'Criar produto',
+                        options: FFButtonOptions(
+                          width: 150,
+                          height: 44,
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                          iconPadding:
+                              EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                          color: Color(0xFFFF8BBB),
+                          textStyle:
+                              FlutterFlowTheme.of(context).bodyMedium.override(
+                                    fontFamily: 'Poppins',
+                                    color: Colors.white,
+                                    letterSpacing: 0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                          elevation: 3,
+                          borderSide: BorderSide(
+                            color: Colors.transparent,
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(100),
+                          hoverColor: Color(0xFF2B16ED),
+                          hoverTextColor: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
